cb578322080efe0c434ad768f8ddd89f3885ba7d
Kevin <kevin123454545@gmail.com>
2023-03-09 23:21:15 +0100

    Added some more accurate tables to the database

--- database/init.sql
+++ database/init.sql
@@ -1,6 +1,29 @@
-CREATE TABLE humans(
+CREATE TABLE accounts(
     id INT PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR(50)
+    username VARCHAR(50),
+    password VARCHAR(50),
+    name VARCHAR(50),
+    FOREIGN KEY (reviewID) REFERENCES reviews(id)-
 );
+
+CREATE TABLE products(
+    id INT PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(50),
+    price INT,
+
+);
+
+CREATE TABLE reviews(
+    id INT PRIMARY KEY AUTOINCREMENT,
+    review VARCHAR(50)
+);
+
+
+CREATE TABLE ratings(
+    id INT PRIMARY KEY AUTOINCREMENT,
+    rate INT
+);
+
+
 
 INSERT INTO humans (name) VALUES ('Alice');
