3dd4115ddace0fb5c29f250fe9ed1472f508ea1f
Kevin Karlsson <kevin123454545@gmail.com>
2023-03-13 12:30:42 +0100

    Added some rows into tables and some test accounts/products

--- database/init.sql
+++ database/init.sql
@@ -3,27 +3,49 @@ CREATE TABLE accounts(
     username VARCHAR(50),
     password VARCHAR(50),
     name VARCHAR(50),
-    FOREIGN KEY (reviewID) REFERENCES reviews(id)-
+    sername VARCHAR(50),
+    FOREIGN KEY (reviewID) REFERENCES reviews(id),
+    FOREIGN KEY (productID) REFERENCES products(id),
+    FOREIGN KEY (ratingID) REFERENCES ratings(id)
 );
 
 CREATE TABLE products(
     id INT PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(50),
     price INT,
-
+    description VARCHAR(150),
+    --imageString VARCHAR(150),
+    tag INT,
+    FOREIGN KEY (accountID) REFERENCES accounts(id)
 );
 
 CREATE TABLE reviews(
     id INT PRIMARY KEY AUTOINCREMENT,
-    review VARCHAR(50)
+    review VARCHAR(50),
+    FOREIGN KEY (accountID) REFERENCES accounts(id)
 );
 
 
 CREATE TABLE ratings(
     id INT PRIMARY KEY AUTOINCREMENT,
-    rate INT
+    rate INT,
+    FOREIGN KEY (accountID) REFERENCES accounts(id)
 );
 
+/*
+Adding a test account and some test products to experiment with
+*/
 
+INSERT INTO accounts(username, password, name, surname) VALUES ('kevve', 'abc123', 'Kevin', 'Karlsson');
 
-INSERT INTO humans (name) VALUES ('Alice');
+INSERT INTO accounts(username, password, name, surname) VALUES ('smoovi', 'abc123', 'Albin', 'Wiklund');
+
+INSERT INTO products(name, price, description, tag, accountID) VALUES ('Matte bok', 150, 'test description', 1, 1)
+
+INSERT INTO products(name, price, description, tag, accountID) VALUES ('Biologi bok', 200, 'test description', 2, 1)
+
+INSERT INTO products(name, price, description, tag, accountID) VALUES ('Fysik bok', 225, 'test description', 3, 1)
+
+INSERT INTO products(name, price, description, tag, accountID) VALUES ('Svenska bok', 300, 'test description', 4, 2)
+
+INSERT INTO products(name, price, description, tag, accountID) VALUES ('Geografi bok', 275, 'test description', 5, 2)
