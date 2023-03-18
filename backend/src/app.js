b93ce58fce217b42e197f51736729aeda7fe1d84
Kevin Karlsson <kevin123454545@gmail.com>
2023-03-18 17:03:51 +0100

    Added some post and get requests in backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,23 +15,178 @@ pool.on('error', function(error){
 
 const app = express()
 
-app.get('/humans', async function(request, response){
-    try{
-        const connection = await pool.getConnection()
+app.get('/allusers', async function(request, response){
+    const connection = await pool.getConnection()
 
-        const query = 'SELECT * FROM humans ORDER BY name'
+    try {
+        const query = 'SELECT * FROM accounts ORDER BY name'
 
-        const humans = await connection.query(query)
+        const accounts = await connection.query(query)
 
-        response.status(200).json(humans)
-    }catch(error){
+        response.status(200).json(accounts)
+    } catch(error) {
         console.log(error)
-        response.status(500).end()
+        response.status(500).end("Internal server error.")
+    } finally {
+        connection.release()
+    }
+})
+
+app.get('/allusers/:id', async function(request, response){
+    const connection = await pool.getConnection()
+
+    try {
+        const query = 'SELECT * FROM accounts WHERE id = ?'
+        
+        const value = [request.params.id]
+
+        const selectedAccount = await connection.query(query, value)
+
+        response.status(200).json(selectedAccount)
+    } catch (error) {
+        console.log(error)
+        response.status(500).end("Internal server error.")
+    } finally {
+        connection.release()
+    }
+})
+
+app.get('/allbooks', async function(request, response){
+    const connection = await pool.getConnection()
+
+    try {
+        const query = 'SELECT * FROM books ORDER BY name'
+
+        const books = await connection.query(query)
+
+        response.status(200).json(books)
+    } catch(error) {
+        console.log(error)
+        response.status(500).end("Internal server error.")
+    } finally {
+        connection.release()
+    }
+})
+
+app.get('/allbooks/:id', async function(request, response){
+    const connection = await pool.getConnection()
+
+    try {
+        const query = 'SELECT * FROM book WHERE id = ?'
+
+        const value = [request.params.id]
+
+        const selectedBook = await connection.query(query, value)
+
+        response.status(200).json(selectedBook)
+    } catch (error) {
+        console.log(error)
+        response.status(500).end("Internal server error.")
+    } finally {
+        connection.release()
+    }
+})
+
+app.post('/sellbook', async function(request, response){
+    const connection = await pool.getConnection()
+
+    try {
+        const query = 'INSERT INTO books(name, price, description, tag, accountID) VALUES (?, ?, ?, ?, ?)'
+
+        const values = [request.body.name, request.body.price, request.body.description, request.body.tag, request.body.id]
+
+        const sellBook = await connection.query(query, values)
+
+        response.status(200).json(sellBook)
+    } catch (error) {
+        console.log(error)
+        response.status(500).end("Internal server error.")
+    } finally {
+        connection.release()
+    }
+})
+
+app.get('/:id', async function(request, response){
+    const connection = await pool.getConnection()
+
+    try {
+        const id = request.params.id
+
+        const query = 'SELECT * FROM accounts WHERE id = ?'
+
+        const account = await connection.query(query, [id])
+
+        response.status(200).json(account)
+    } catch(error) {
+        console.log(error)
+        response.status(500).end("Internal server error.")
+    } finally {
+        connection.release()
+    }
+
+})
+
+app.post('/signup', async function(request, response){
+    const connection = await pool.getConnection()
+
+    try {
+        const query = 'INSERT INTO accounts(username, password, name, surname) VALUES (?, ?, ?, ?)'
+
+        const values = [request.body.username, request.body.password, request.body.name, request.body.surname]
+
+        const signUpAccount = await connection.query(query, values)
+
+        response.status(200).json(signUpAccount)
+    } catch (error) {
+        console.log(error)
+        response.status(500).end("Internal server error")
+    } finally {
+        connection.release()
+    }
+
+})
+
+app.post('/signin', async function(request, response){
+    const connection = await pool.getConnection()
+
+    try {
+        const query = 'SELECT * FROM accounts WHERE username = ? AND password = ?'
+
+        const values = [request.body.username, request.body.password]
+
+        const signInAccount = await connection.query(query, values)
+
+        response.status(200).json(signInAccount,)
+    } catch (error) {
+        console.log(error)
+        response.status(500).end("Internal server error.")
+    } finally {
+        connection.release()
+    }
+})
+
+app.get('/accounts', async function(request, response){
+    const connection = await pool.getConnection()
+
+    try {
+        const query = 'SELECT * FROM accounts ORDER BY name'
+
+        const accounts = await connection.query(query)
+
+        response.status(200).json(accounts)
+    } catch(error) {
+        console.log(error)
+        response.status(500).end("Internal server error.")
+    } finally {
+        connection.release()
     }
 })
 
 app.get('/', function(request, response){
+    console.log("Hola!")
     response.send('It works!')
 })
+
+console.log("listening on 80808")
 
 app.listen(8080)
--- database/init.sql
+++ database/init.sql
@@ -1,51 +1,26 @@
-CREATE TABLE accounts(
-    id INT PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE IF NOT EXISTS accounts(
+    id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(50),
     password VARCHAR(50),
     name VARCHAR(50),
-    sername VARCHAR(50),
-    FOREIGN KEY (reviewID) REFERENCES reviews(id),
-    FOREIGN KEY (productID) REFERENCES products(id),
-    FOREIGN KEY (ratingID) REFERENCES ratings(id)
+    surname VARCHAR(50)
 );
 
-CREATE TABLE products(
-    id INT PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE IF NOT EXISTS books(
+    id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50),
     price INT,
     description VARCHAR(150),
-    --imageString VARCHAR(150),
     tag INT,
+    accountID INT,
     FOREIGN KEY (accountID) REFERENCES accounts(id)
 );
 
-CREATE TABLE reviews(
-    id INT PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE IF NOT EXISTS reviews(
+    id INT PRIMARY KEY AUTO_INCREMENT,
     review VARCHAR(50),
+    rating INT,
+    accountID INT,
     FOREIGN KEY (accountID) REFERENCES accounts(id)
 );
 
-
-CREATE TABLE ratings(
-    id INT PRIMARY KEY AUTOINCREMENT,
-    rate INT,
-    FOREIGN KEY (accountID) REFERENCES accounts(id)
-);
-
-/*
-Adding a test account and some test products to experiment with
-*/
-
-INSERT INTO accounts(username, password, name, surname) VALUES ('kevve', 'abc123', 'Kevin', 'Karlsson');
-
-INSERT INTO accounts(username, password, name, surname) VALUES ('smoovi', 'abc123', 'Albin', 'Wiklund');
-
-INSERT INTO products(name, price, description, tag, accountID) VALUES ('Matte bok', 150, 'test description', 1, 1)
-
-INSERT INTO products(name, price, description, tag, accountID) VALUES ('Biologi bok', 200, 'test description', 2, 1)
-
-INSERT INTO products(name, price, description, tag, accountID) VALUES ('Fysik bok', 225, 'test description', 3, 1)
-
-INSERT INTO products(name, price, description, tag, accountID) VALUES ('Svenska bok', 300, 'test description', 4, 2)
-
-INSERT INTO products(name, price, description, tag, accountID) VALUES ('Geografi bok', 275, 'test description', 5, 2)
